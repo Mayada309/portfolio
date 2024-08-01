1f7f0c06cc01f8dde16421f96d5185c5bbb4b0f4
Mayada <mayadaelsayed383@gmail.com>
2024-08-01 12:10:03 +0400

    adjsut about section responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import SectionTitle from './SectionTitle';
 const About = () => {
 	return (
 		<section className='bg-white py-20' id='about'>
-			<div className='align-element grid md: grid-cols-2 items-center gap-16 '>
+			<div className='align-element grid md:grid-cols-2 items-center gap-16 '>
 				<img src={aboutSvg} className='w-full h-64' />
 				<article>
 					<SectionTitle text={'code and coffee'} />
